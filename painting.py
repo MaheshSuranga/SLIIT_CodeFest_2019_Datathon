73857c136216dec78436b4556c9c58af646c941d
MaheshSuranga <mahesh.15@cse.mrt.ac.lk>
2019-10-06 15:14:10 +0530

    Refactor the code

--- painting.py
+++ painting.py
@@ -2,8 +2,8 @@ color = [[12,23,14],[10,30,15],[16,22,35],[14,24,20]]
 
 def find_buckets(A):
     num_of_shades = 3
-    minimum_wastage = 112
-    I,J,K,L = 0,0,0,0
+    minimum_wastage = float("inf")
+    I,J,K,L = -1,-1,-1,-1
     for i in range (num_of_shades):
         for j in range (num_of_shades):
             for k in range (num_of_shades):
@@ -15,9 +15,13 @@ def find_buckets(A):
                     elif(wastage < minimum_wastage):
                         minimum_wastage = wastage
                         I,J,K,L = i,j,k,l
-    print("minimum_wastage for the area {:d} is {:d}".format(A, minimum_wastage))
-    print("relevent indices are [(0,{:d}), (1,{:d}), (2,{:d}), (3,{:d})]".format(I,J,K,L))
 
-                    
+    if(I>-1 and J>-1 and K>-1 and L>-1):
+        print("minimum_wastage for the area {:d} is {:.0f}".format(A, minimum_wastage))
+        print("relevent indices are [(0,{:d}), (1,{:d}), (2,{:d}), (3,{:d})] \n".format(I,J,K,L))
+    else:
+        print("Sorry!!! Amount of paints are not enough for paint the whole area")
+        
+                        
 find_buckets(100)
 find_buckets(90)
